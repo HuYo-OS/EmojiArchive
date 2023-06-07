7354909a302988b820bff585692d0d1ec953925d
HuYo-OS <HOYO-OS@outlook.com>
2023-06-07 21:16:47 +0800

    2023年6月7日

--- .github/update.py
+++ .github/update.py
@@ -1,28 +1,32 @@
 import json
 from pathlib import Path
 
-
 root_path = Path(__file__).parent.parent.resolve()
 ignored_dirs = {'.github', '.git', '准备中2', 'node_modules'}
 ignored_files = {'info.json'}
 
-path_data = {
-    dir.name: {
-        file.stem: str(file.relative_to(root_path))
-            .replace('\\', '/')
-        for file in dir.rglob('*')
-            if (file.is_file() and file.name not in ignored_files
-                and not any(p.name.startswith('.')
-                or p.name in ignored_dirs for p in file.parents))
-    }
-    for dir in root_path.iterdir()
-        if dir.is_dir() and dir.name not in ignored_dirs
-}
+image_extensions = ['.png', '.jpg', '.jpeg', '.gif']
 
-with open(root_path / 'path.json', 'w', encoding='utf-8') as file:
-    json.dump(
-        path_data,
-        file,
-        ensure_ascii=False,
-        indent=2
-    )
+def process_directory(directory):
+    items = []
+    for file in directory.iterdir():
+        if file.is_file() and file.suffix.lower() in image_extensions and file.name not in ignored_files:
+            items.append(str(file.name))
+
+    if items:
+        directory_name = directory.name
+        icon_name = f"{directory_name}.png"
+        info = {
+            'name': directory_name,
+            'icon': icon_name,
+            'items': items
+        }
+        info_json_path = directory / 'info.json'
+        with open(info_json_path, 'w', encoding='utf-8') as file:
+            json.dump(info, file, ensure_ascii=False, indent=2)
+
+    for subdirectory in directory.iterdir():
+        if subdirectory.is_dir() and subdirectory.name not in ignored_dirs:
+            process_directory(subdirectory)
+
+process_directory(root_path)
